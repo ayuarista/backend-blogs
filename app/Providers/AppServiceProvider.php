6c696df4fc2ca7210804e0afd8659b65b37838b2
ayuarista <aristaadewi8@gmail.com>
2025-08-13 10:02:26 +0800

    debug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,11 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (app()->environment('production')) {
-            if (request()->header('X-Forwarded-Proto') === 'https') {
-                URL::forceScheme('https');
-            }
-        }
-
+    
     }
 }
